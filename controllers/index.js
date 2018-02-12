45adbf0a51d7888adc89c11bbaef61943fea8673
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-12 17:30:56 +0000

    data issue

--- controllers/index.js
+++ controllers/index.js
@@ -1,127 +1,107 @@
 const d3 = Object.assign({}, require('d3-array'), require('d3-collection'));
-var jsonData = require("../public/master.json")
+var jsonData = require('../public/master.json');
 
-var express = require("express");
+var express = require('express');
 var router = new express.Router();
 var path = require('path');
 
-router.use("/location", router)
+router.use('/location', router);
 
 var result;
 var cleanTree;
 const newData = {};
+var myData = []
 
-router.get("/", function(req, res){
-  cleanedTree = jsonData.map(k => {
-    return {
-      project: k['Project title'],
-      pillar: k['NDP Pillar'],
-      sector: k['Primary Sector'],
-      disbursements2016: k["2016 Disbursements (USD)"]
-    };
-  });
+router.get('/', function(req, res) {
+	var jsonData = require('../public/master.json');
 
-  result = jsonData.reduce(function (r, o) {
-    Object.keys(o).forEach(function (k)  {
-      if (['project', 'pillar', 'sector'].includes(k) || !o[k]) {
-        return;
-      }
-      r.push({ location: k, value: o[k], name: o.name, 'date started': o['date started'] });
-    });
-    console.log(r);
-    return r;
-  }, []);
+	// return only data needed
+	// simplify keys
+	// make separate variables for each year
+	const cleanedTree2016 = jsonData.map(k => {
+		return {
+			project: k['Project title'],
+			pillar: k['NDP Pillar'],
+			sector: k['Primary Sector'],
+			// disbursements2016: k['2016 Disbursements (USD)'],
+			fgs: k['2016 - FGS'],
+			benadir: k['2016 - Benadir'],
+			galmudug: k['2016 - Galmudug'],
+			hiirshabelle: k['2016 - Hiirshabelle'],
+			jubaland: k['2016 - Jubaland'],
+			puntland: k['2016 - Puntland'],
+			southWest: k['2016 - South West'],
+			somaliland: k['2016 - Somaliland'],
+			unattributed: k['2016 - Unattributed'],
+		};
+	});
 
+	// console.log('nice and tidy map: ', cleanedTree2016);
 
-  newData = d3
-  .nest()
-  .key(d => d.pillar)
-  .key(d => d.sector)
-  .key(d => d.project)
-  .rollup(d => d3.sum(d, d => d.disbursements2016))
-  .entries(cleanTree);
-  tree2016 = [
-    {
-      name: 2016,
-      children: tree2016.values.map(pillar => {
-        return {
-          name: pillar.key,
-          children: component.values.map(sector => {
-            return {
-              name: sector.key,
-              children: sector.values.map(project => {
-                return {
-                  name: project.key,
-                  value: project.value,
-                };
-              }),
-            };
-          }),
-        };
-      }),
-    }]
+	// create a new object for each project that has a location-specific value greater than zero.
+	cleanedTree2016.reduce(function(r, o) {
+		Object.keys(o).forEach(function(k) {
+			if (['project', 'pillar', 'sector'].includes(k) || !o[k]) {
+				return;
+			}
+			r.push({
+				location: k,
+				value: o[k],
+				project: o.project,
+				pillar: o.pillar,
+				sector: o.sector,
+			});
+		});
 
+		// console.log('brilliant awesome german function: ', r);
+		myData.push(r)
+		return r;
+	}, []);
 
-    res.json(newData)
-  })
+	// console.log(myData);
 
+	// Russian doll this shit
+	const nested2016 = d3
+	.nest()
+	.key(d => d.location)
+	.key(d => d.pillar)
+	.key(d => d.sector)
+	.key(d => d.project)
+	.rollup(d => d3.sum(d, d => d.value))
+	.entries(cleanedTree2016);
 
-  class TreeMap {
-    constructor(data) {
-      this.data = jsonData
-    }
-  }
+	// console.log(nested2016);
+	console.log('nested: ', nested2016);
 
+	console.log(myData.length);
 
-  TreeMap.prototype.cleanTree = function(data) {
-    this.cleanedTree = data.map(k => {
-      return {
-        project: k['Project Title'],
-        pillar: k['Pillar'],
-        sector: k['Primary Sector'],
-      };
-    });
-  },
+const tree2016 = [
+	{
+		name: 2016,
+		children: myData.values.map(location => {
+			return {
+				name: location.key,
+				children: location.values.map(pillar => {
+					return {
+						name: pillar.key,
+						children: pillar.values.map(sector => {
+							return {
+								name: sector.key,
+								children: sector.values.map(project => {
+									return {
+										name: project.key,
+										value: project.value,
+									};
+								}),
+							};
+						}),
+					};
+				}),
+			};
+		})
+	}
+];
 
-  TreeMap.prototype.setTrees = function(data) {
-
-    SET_TREES: (state, data) => {
-      state.tree2017.values = d3
-      .nest()
-      .key(d => d.pillar)
-      .key(d => d.component)
-      .key(d => d.sector)
-      .key(d => d.project)
-      .rollup(d => d3.sum(d, d => d.usd2017))
-      .entries(state.cleanTree);
-      state.tree2017 = [
-        {
-          name: 2017,
-          children: state.tree2017.values.map(pillar => {
-            return {
-              name: pillar.key,
-              children: pillar.values.map(component => {
-                return {
-                  name: component.key,
-                  children: component.values.map(sector => {
-                    return {
-                      name: sector.key,
-                      children: sector.values.map(project => {
-                        return {
-                          name: project.key,
-                          value: project.value,
-                          url: `http://suiviaide-rca.cf/projects/${project.key}`,
-                        };
-                      }),
-                    };
-                  }),
-                };
-              }),
-            };
-          }),
-        },
-      ];
-    }
-  }
-
-  module.exports = router;
+res.json(newData);
+});
+module.exports = router;
