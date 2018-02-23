488c579323b8830863631adb225498434ee917e8
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-23 10:52:38 +0000

    route page set up

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var bodyParser = require("body-parser");
 var jsonData = require("./public/cleaned-master")
 var pooled = require("./public/pooled")
 var envelopes = require("./public/envelopes")
+var path = require('path')
 
 var TreeMap2016 = require('./controllers/TreeMap2016')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(require('./controllers/Locations'));
 
 
@@ -16,8 +18,8 @@ app.use(require('./controllers/TreeMap2016'));
 app.use(require('./controllers/TreeMap2017'));
 app.use(require('./controllers/TreeMap2018'));
 
-app.get("/", function(req, res){
-  res.json({"Hello": "World"})
+app.get("/routes", function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get("/pooled", function(req, res){
