65f134a52a6406944c9625a6c50e98e7f9bdf5da
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-26 10:00:21 +0000

    Changed locations route

--- controllers/Locations.js
+++ controllers/Locations.js
@@ -9,12 +9,20 @@ router.use('/locations', router);
 var valid_place_names = ["FGS", "Benadir", "Galmudug", "Hiirshabelle", "Jubaland", "Puntland", "South West", "Somaliland", "Unattributed"];
 var years = ["2016", "2017", "2018"];
 var placeArray;
+var temp_array = []
 
 router.get('/', function(req, res) {
 
-
   placeArray = []
   for (var location of valid_place_names) {
+     total2016 = 0;
+     total2017 = 0;
+     total2018 = 0;
+
+     count2016 = 0;
+     count2017 = 0;
+     count2018 = 0;
+
     yearLoop(location)
   }
 
@@ -30,25 +38,29 @@ var locationLoop = function() {
 var createCountryObject = function(location, year) {
   var jsonData = require('../public/cleaned-master.json');
 
-  var total = 0;
-  var count = 0;
-
   for (var i = 0; i < 770; i++) {
-    if (jsonData[i][year+" - "+location] > 0) {
-      count += 1;
-      total += jsonData[i][year+" - "+location]
+    if (year === "2016" && jsonData[i][year+" - "+location] > 0) {
+      count2016 += 1;
+      total2016 += jsonData[i][year+" - "+location]
+    } else if (year === "2017" && jsonData[i][year+" - "+location] > 0) {
+      count2017 += 1;
+      total2017 += jsonData[i][year+" - "+location]
+    } else if (year === "2018" && jsonData[i][year+" - "+location] > 0) {
+      count2018 += 1;
+      total2018 += jsonData[i][year+" - "+location]
     }
   }
 
-  var totalKey = "Total"+year
-  var countKey = "Count"+year
   temp_obj = {
+    "location": location,
+    "total2016": total2016,
+    "count2016": count2016,
+    "total2017": total2017,
+    "count2017": count2017,
+    "total2018": total2018,
+    "count2018": count2018
 
-    [totalKey] : total,
-    [countKey] : count
   }
-
-  temp_array.push(temp_obj)
 }
 
 var yearLoop = function(location) {
@@ -57,7 +69,7 @@ var yearLoop = function(location) {
   for (var year of years) {
     createCountryObject(location, year)
   }
-  placeArray.push( { [location] : temp_array } )
+  placeArray.push(temp_obj)
 }
 
 module.exports = router;
